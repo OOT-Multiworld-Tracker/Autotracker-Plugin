8bb7d1646ee17f7fa8ef4d7edd0102b7794e4ad0
Ben <bennyman123abc@hotmail.com>
2021-10-20 05:34:10 -0500

    Implment Scrub tracking for Scrubsanity

--- src/autotracker-plugin/autotracker_plugin.ts
+++ src/autotracker-plugin/autotracker_plugin.ts
@@ -1,42 +1,53 @@
 import {IPlugin, IModLoaderAPI, ILogger} from 'modloader64_api/IModLoaderAPI';
-import {AmmoUpgrade, IInventory, IOOTCore, LinkState, OotEvents} from 'modloader64_api/OOT/OOTAPI';
+import {IOOTCore, LinkState, OotEvents} from 'modloader64_api/OOT/OOTAPI';
 import {InjectCore} from 'modloader64_api/CoreInjection';
-import { bus, EventHandler, EventsClient } from 'modloader64_api/EventHandler';
+import { bus, EventHandler } from 'modloader64_api/EventHandler';
 import { Server } from 'ws';
 
 import { NetworkHandler, } from 'modloader64_api/NetworkHandler';
 import { Packet } from 'modloader64_api/ModLoaderDefaultImpls';
 
+import { JSONTemplate } from 'modloader64_api/JSONTemplate';
+import { IActor } from 'modloader64_api/OOT/IActor';
 
-enum PacketStates {
-    INVENTORY_STATE,
-    SCENE_STATE
-}
-
-enum AutotrackerEvents {
+enum AutotrackerEvent {
     ON_CHEST_OPENED = 'Autotracker:onChestOpened',
     ON_COLLECTABLE_GATHERED = 'Autotracker:onCollectableGathered',
     ON_SKULLTULA_GATHERED = 'Autotracker:onSkulltulaGathered',
+    ON_SAVE_EVENT = 'Autotracker:onSaveEvent',
+    ON_PERM_SCENE_DATA_CHANGE = 'Autotracker:onPermSceneDataChange',
 }
 
-enum AutotrackerPayloads {
+enum AutotrackerPayload {
     CONNECTED_SAVE_REQUEST,
     NOT_INITALIZED,
     TRACKER_UPDATE,
 }
 
-enum MultiworldTrackerPayloads {
+enum MultiworldTrackerPayload {
     SEND_SAVE,
     SEND_SCENE,
     SEND_CHEST_CHANGE,
     SEND_COLLECTABLE_CHANGE,
     SEND_SKULLTULA_CHANGE,
-    UNUSED,
+    SEND_EVENT_CHANGE,
+    SEND_OTHER_TRACKER,
+    SEND_SCRUB_BUY,
 }
 
-type AutotrackerData = {
-    index: number,
-    object: number,
+class AutotrackerData extends JSONTemplate {
+    index: number = 0;
+    object: number = 0;
+
+    constructor() {
+        super();
+        this.jsonFields = ["index", "object"];
+    }
+
+    toString(): string {
+        var s = JSON.stringify(this.toJSON());
+        return s;
+    }
 }
 
 export class TrackerUpdate extends Packet {
@@ -58,12 +69,19 @@ class autotracker_plugin implements IPlugin {
     @InjectCore()
     core!: IOOTCore;
 
+    previousRupees!: number;
+
     previousChestState!: Buffer;
     previousCollectableState!: Buffer;
     previousSkulltulaState!: Buffer;
-    prepareItemSend: boolean = false;
-    skulltula: AutotrackerData = {index: 0, object: 0};
+    previousEventState!: Buffer;
+
+    previousSwitchState!: Buffer;
+    previousPermSceneState!: Buffer;
+
     skulltulaGathered: boolean = false;
+    prepareItemSend: boolean = false;
+
     saveInit: boolean = false;
     lastPacket!: object;
     wss!: Server;
@@ -80,16 +98,16 @@ class autotracker_plugin implements IPlugin {
             socket.on('message', (data) => {
                 let json = JSON.parse(data.toString());
                 switch (json["PAYLOAD"]) {
-                    case AutotrackerPayloads.CONNECTED_SAVE_REQUEST:
+                    case AutotrackerPayload.CONNECTED_SAVE_REQUEST:
                         this.ModLoader.logger.info(`Sent current game-state for tracker request`);
                         let payload = {}
                         this.sendState(0, {save: this.core.save})
                         break
 
-                    case AutotrackerPayloads.NOT_INITALIZED:
+                    case AutotrackerPayload.NOT_INITALIZED:
                         socket.send("NOT_INITALIZED")
                         break
-                    case AutotrackerPayloads.TRACKER_UPDATE:
+                    case AutotrackerPayload.TRACKER_UPDATE:
                         // let json = JSON.parse(data.toString()) // Unused code??
                         this.ModLoader.clientSide.sendPacket(new TrackerUpdate(data.toString(), this.ModLoader.clientLobby))
                         break
@@ -97,9 +115,8 @@ class autotracker_plugin implements IPlugin {
             })
         })
     }
-    postinit(): void {
-        
-    }
+
+    postinit(): void {}
 
     onTick(frame?: number | undefined): void {
         if (!this.core.link.exists) { return }
@@ -109,34 +126,15 @@ class autotracker_plugin implements IPlugin {
         if (frame !== undefined && frame % 20 == 0) {
             if (this.core.link.state == (LinkState.BUSY || LinkState.LOADING_ZONE)) return;
 
-            this.previousSkulltulaState.forEach((v, i) => {
-                var cur = this.core.save.skulltulaFlags;
-                if (this.skulltula.object) return;
-                if ((v ^ cur[i]) != 0)
-                    this.skulltula = {index: i, object: (v ^ cur[i])};
-            });
-
-            if (this.skulltula.object) {
-                bus.emit(AutotrackerEvents.ON_SKULLTULA_GATHERED, {index: this.skulltula.index, skulltula: this.skulltula.object});
-                this.previousSkulltulaState = this.core.save.skulltulaFlags;
-                this.skulltula = {index: 0, object: 0};
-            }
+            this.testItemState(this.previousSkulltulaState, MultiworldTrackerPayload.SEND_SKULLTULA_CHANGE);
+            this.testItemState(this.previousCollectableState, MultiworldTrackerPayload.SEND_COLLECTABLE_CHANGE);
         }
 
-        if (this.core.link.state == LinkState.BUSY) this.prepareItemSend = true;
+        if (this.core.link.state == LinkState.GETTING_ITEM) this.prepareItemSend = true;
 
-        // TODO: Re-write to test against all bytes
-        // Run events checks after the Item Gathered animation finishes
         if (this.prepareItemSend && this.core.link.state == LinkState.STANDING) {
-            var chestOpened = this.core.global.liveSceneData_chests.toJSON().data[3] ^ this.previousChestState.toJSON().data[3];
-            var collectableGathered = this.core.global.liveSceneData_collectable.toJSON().data[0] ^ this.previousCollectableState.toJSON().data[0];
-
-            if (chestOpened) bus.emit(AutotrackerEvents.ON_CHEST_OPENED, chestOpened);
-            else if (collectableGathered) bus.emit(AutotrackerEvents.ON_COLLECTABLE_GATHERED, collectableGathered);
-            else this.ModLoader.logger.error("Nothing was collected. This is probably because of the new handler.");
-
-            this.previousChestState = this.core.global.liveSceneData_chests;
-            this.previousCollectableState = this.core.global.liveSceneData_collectable;
+            this.testItemState(this.previousChestState, MultiworldTrackerPayload.SEND_CHEST_CHANGE);
+            this.testItemState(this.previousEventState, MultiworldTrackerPayload.SEND_EVENT_CHANGE);
 
             this.prepareItemSend = false;
         }
@@ -146,7 +144,7 @@ class autotracker_plugin implements IPlugin {
     onClientItemGet(packet: TrackerUpdate): void
     {
         var data: string = packet.data
-        this.sendState(3, JSON.parse(data).data)
+        this.sendState(MultiworldTrackerPayload.SEND_OTHER_TRACKER, JSON.parse(data).data)
     }
 
     @EventHandler(OotEvents.ON_SAVE_LOADED)
@@ -156,7 +154,11 @@ class autotracker_plugin implements IPlugin {
         this.sendState(0, {save: this.core.save})
 
         this.previousSkulltulaState = this.core.save.skulltulaFlags;
+        this.previousEventState = this.core.save.eventFlags;
 
+        this.previousPermSceneState = this.core.save.permSceneData;
+
+        // DEBUG: Used for skulltula testing
         // this.core.save.inventory.bombs = true;
         // this.core.save.inventory.bombBag = AmmoUpgrade.MAX;
         // this.core.save.inventory.bombsCount = 40;
@@ -165,7 +167,10 @@ class autotracker_plugin implements IPlugin {
         // this.core.save.inventory.dekuSticksCapacity = AmmoUpgrade.MAX;
         // this.core.save.inventory.dekuSticksCount = 30;
 
-        // this.core.commandBuffer.runWarp(0x138, 16, ()=>{})
+        // this.core.commandBuffer.runWarp(0x5A4, 16, ()=>{})
+        // this.core.save.shields.dekuShield = true;
+        // this.core.save.swords.kokiriSword = true;
+        // this.core.save.rupee_count = 99;
     }
 
     @EventHandler(OotEvents.ON_SCENE_CHANGE)
@@ -177,6 +182,9 @@ class autotracker_plugin implements IPlugin {
         this.previousChestState = this.core.global.liveSceneData_chests;
         this.previousCollectableState = this.core.global.liveSceneData_collectable;
         this.previousSkulltulaState = this.core.save.skulltulaFlags;
+
+        // DEBUG Switch state
+        this.previousSwitchState = this.core.global.liveSceneData_switch;
     }
 
     @EventHandler(OotEvents.ON_HEALTH_CHANGE)
@@ -185,25 +193,49 @@ class autotracker_plugin implements IPlugin {
         this.sendSaveState();
     }
 
-    @EventHandler(AutotrackerEvents.ON_CHEST_OPENED)
-    onChestOpened(chestOpened) {
+    @EventHandler(AutotrackerEvent.ON_CHEST_OPENED)
+    onChestOpened(chestOpened: AutotrackerData) {
         this.ModLoader.logger.debug("Sending save packet (Chest Update)");
         this.sendSaveState();
         this.ModLoader.logger.debug(`Chest Opened: ${chestOpened.toString()}`);
         this.sendChestState(chestOpened)
     }
 
-    @EventHandler(AutotrackerEvents.ON_COLLECTABLE_GATHERED)
-    onCollectableGathered(collectableGathered) {
+    @EventHandler(AutotrackerEvent.ON_COLLECTABLE_GATHERED)
+    onCollectableGathered(collectableGathered: AutotrackerData) {
         this.ModLoader.logger.debug("Sending save packet (Collectable Update)");
         this.sendSaveState();
-
+        this.ModLoader.logger.debug(`Collectable Gathered: ${collectableGathered.toString()}`);
+        this.sendCollectableState(collectableGathered);
     }
 
-    @EventHandler(AutotrackerEvents.ON_SKULLTULA_GATHERED)
-    onSkulltulaGathered(skulltula) {
-        this.ModLoader.logger.debug(JSON.stringify(skulltula));
+    @EventHandler(AutotrackerEvent.ON_SKULLTULA_GATHERED)
+    onSkulltulaGathered(skulltula: AutotrackerData) {
+        this.ModLoader.logger.debug("Sending save packet (Skulltula Update)");
+        this.sendSaveState();
+        this.ModLoader.logger.debug(`Skulltula Gathered: ${skulltula.toString()}`);
+        this.sendSkulltulaState(skulltula);
+    }
 
+    @EventHandler(AutotrackerEvent.ON_SAVE_EVENT)
+    onSaveEvent(event: AutotrackerData) {
+        this.ModLoader.logger.debug("Sending save packet (Event Update)");
+        this.sendSaveState();
+        this.ModLoader.logger.debug(`Event Changed: ${event.toString()}`);
+        this.sendEventState(event);
+    }
+
+    @EventHandler(OotEvents.ON_ACTOR_SPAWN)
+    onActorSpawn(actor: IActor) {
+        if (actor.actorType == 1 && actor.actorID == 282) {
+            this.previousRupees = this.core.save.rupee_count;
+        }
+    }
+
+    @EventHandler(OotEvents.ON_ACTOR_DESPAWN)
+    onActorDespawn(actor: IActor) {
+        if (actor.actorType == 1 && actor.actorID == 282 && this.previousRupees != this.core.save.rupee_count)
+            this.sendState(MultiworldTrackerPayload.SEND_SCRUB_BUY, {scene: this.core.global.scene, data: `[${actor.position.getRawPos().toJSON().data.toString()}]`})
     }
 
     sendState(payload: number, state: object) {
@@ -213,19 +245,83 @@ class autotracker_plugin implements IPlugin {
     }
 
     sendSaveState() {
-        this.sendState(MultiworldTrackerPayloads.SEND_SAVE, {save: this.core.save})
+        this.sendState(MultiworldTrackerPayload.SEND_SAVE, {save: this.core.save})
     }
 
-    sendChestState(chestOpened) {
-        this.sendState(MultiworldTrackerPayloads.SEND_CHEST_CHANGE, { scene: this.core.global.scene, chest: chestOpened })
+    sendChestState(chestOpened: AutotrackerData) {
+        this.sendState(MultiworldTrackerPayload.SEND_CHEST_CHANGE, { scene: this.core.global.scene, data: chestOpened.toString() })
     }
 
-    sendCollectableState(collectableGathered) {
-        this.sendState(MultiworldTrackerPayloads.SEND_CHEST_CHANGE, { scene: this.core.global.scene, collectable: collectableGathered})
+    sendCollectableState(collectableGathered: AutotrackerData) {
+        this.sendState(MultiworldTrackerPayload.SEND_COLLECTABLE_CHANGE, { scene: this.core.global.scene, data: collectableGathered.toString() })
     }
 
-    sendSkulltulaState(skulltulaGathered) {
-        this.sendState(MultiworldTrackerPayloads.SEND_SKULLTULA_CHANGE, { skulltula: skulltulaGathered })
+    sendSkulltulaState(skulltulaGathered: AutotrackerData) {
+        this.sendState(MultiworldTrackerPayload.SEND_SKULLTULA_CHANGE, { data: skulltulaGathered.toString() })
+    }
+
+    sendEventState(eventChanged: AutotrackerData) {
+        this.sendState(MultiworldTrackerPayload.SEND_EVENT_CHANGE, { data: eventChanged.toString() })
+    }
+
+    testItemState(previousState: Buffer, payload: MultiworldTrackerPayload) {
+        var item: AutotrackerData = new AutotrackerData();
+
+        switch (payload) {
+            case MultiworldTrackerPayload.SEND_CHEST_CHANGE:
+                var currentState = this.core.global.liveSceneData_chests;
+                var event: AutotrackerEvent = AutotrackerEvent.ON_CHEST_OPENED;
+                break;
+
+            case MultiworldTrackerPayload.SEND_SKULLTULA_CHANGE:
+                var currentState = this.core.save.skulltulaFlags;
+                var event: AutotrackerEvent = AutotrackerEvent.ON_SKULLTULA_GATHERED;
+                break;
+
+            case MultiworldTrackerPayload.SEND_COLLECTABLE_CHANGE:
+                var currentState = this.core.global.liveSceneData_collectable;
+                var event: AutotrackerEvent = AutotrackerEvent.ON_COLLECTABLE_GATHERED;
+                break;
+
+            case MultiworldTrackerPayload.SEND_EVENT_CHANGE:
+                var currentState = this.core.save.eventFlags;
+                var event: AutotrackerEvent = AutotrackerEvent.ON_SAVE_EVENT;
+                break;
+
+            default:
+                return;
+        }
+
+        previousState.forEach((v, i) => {
+            if (item.object) return;
+            if ((v ^ currentState[i]) != 0) {
+                item.index = i;
+                item.object = v ^ currentState[i];
+            }
+        });
+
+        if (item.object) {
+            bus.emit(event, item);
+            previousState = currentState;
+
+            switch (payload) {
+                case MultiworldTrackerPayload.SEND_CHEST_CHANGE:
+                    this.previousChestState = currentState;
+                    break;
+
+                case MultiworldTrackerPayload.SEND_SKULLTULA_CHANGE:
+                    this.previousSkulltulaState = currentState;
+                    break;
+
+                case MultiworldTrackerPayload.SEND_COLLECTABLE_CHANGE:
+                    this.previousCollectableState = currentState
+                    break;
+                
+                case MultiworldTrackerPayload.SEND_EVENT_CHANGE:
+                    this.previousEventState = currentState;
+                    break;
+            }
+        }
     }
 }
 
